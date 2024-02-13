78cc2847e48c8c28ef3bb16dd96c2e88e6a56f53
Bastodon <superbastodon@gmail.com>
2024-02-13 16:30:15 +0100

    Configuration de Angular Materials. Le module n'est pas correctement importé dans le projet et ne peut pas encore être exploité dans les components.html. Mise en commentaire de commandes et balises de tests dans app.component.html. Création du fichier app.module.ts qui n'existait pas auparavant. Ajout d'une ligne d'import dans main.ts et dans app.component.ts (aucun des deux ne semble faire fonctionner l'import. Peut-être il manque un appel dans une commande ?)
    Signed-off-by: Bastodon <superbastodon@gmail.com>

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,16 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { NgModule } from '@angular/core';
+
+@NgModule ({
+  imports: [
+    MatSlideToggleModule,
+  ]
+})
+class AppModule {}
+
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
 
