7d75c576d7de84bfdc9665cc6af4500daf917723
Bastodon <superbastodon@gmail.com>
2024-02-13 15:39:01 +0100

    Retrait du code test dans index.html. Commentaires pour l'utilité des fichiers principaux (index.html, main.ts, styles.css)

--- src/main.ts
+++ src/main.ts
@@ -4,3 +4,5 @@ import { AppComponent } from './app/app.component';
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
+
+  // Ici seront lancés les modules d'Angular, pour avoir accès à toutes les fonctionnalités des components par exemple //
