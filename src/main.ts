08d600ffc14f31525cc82706d12d7eb3a1f8e986
Bastodon <superbastodon@gmail.com>
2024-02-14 12:33:20 +0100

    Après étude, il semblerait que Angular ait initialisé le projet en "standalone". Ce qui signifie que les modules ne peuvent pas être ajoutés. Comme ils ne sont pas une partie nécessaire du projet, je choisis de m'en passer pour cette fois. Les appels de classe de Angular Material sont retirés.

--- src/main.ts
+++ src/main.ts
@@ -14,5 +14,3 @@ class AppModule {}
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
-
-  // Ici seront lancés les modules d'Angular, pour avoir accès à toutes les fonctionnalités des components par exemple //
